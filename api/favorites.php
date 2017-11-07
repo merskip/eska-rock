2f6e71ac901cb009bdbfd0024b301610929ad205
Piotr Merski <merskip@gmail.com>
2017-11-07 18:08:01 +0100

    Support for only unique favorites songs per user in database

--- api/favorites.php
+++ api/favorites.php
@@ -16,7 +16,14 @@ else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     isset($_POST["songTitle"]) or die("Argument songTitle is required");
 
     $songTitle = $_POST["songTitle"];
-    $id = $favorites->insertFavoriteSong($songTitle);
+
+    if ($favoriteSong = $favorites->findFavoriteSong($songTitle)) {
+        $id = $favoriteSong->_id;
+    }
+    else {
+        $id = $favorites->insertFavoriteSong($songTitle);
+        http_response_code(201); // 201 - Created
+    }
 
     header("Content-Type: application/json");
     echo json_encode(["_id" => $id], JSON_PRETTY_PRINT);
